c72a7a9472d84dcb086de59398eee71dd9691a81
Rishav Ray <rishav.rray@gmail.com>
2023-02-04 17:13:56 +0100

    PILFER pandas inplementation

--- workflow/scripts/pilfer.py
+++ workflow/scripts/pilfer.py
@@ -2,6 +2,7 @@
 import numpy as np
 from operator import itemgetter
 import sys, csv, os, argparse
+import pandas as pd
 
 
 parser = argparse.ArgumentParser(usage="pilfer.py [-h] [--factor <value>] -i input.BED", description="Predicts piRNA clusters from an input BED file with the reads and the raw read counts in the 5th column.\nBy default output is printed in stardard output")
@@ -13,10 +14,81 @@ args = parser.parse_args()
 def column(matrix, i):
 	return [row[i] for row in matrix]
 
+def read_data_pandas(infile_pt):
+	map = pd.read_csv(infile_pt, header=0, sep='\t', names = ['chr', 'start', 'end', 'pir', 'rpm', 'strand', 'count'])
+
+	rpm_fact = map['count'].sum()/1000000
+	map['rpm'] = map['count']*rpm_fact
+	#print(map.head())
+	mean = map['rpm'].mean()
+	sd = map['rpm'].std()
+	#print(rpm_fact,mean,sd)
+	map = map.values.tolist()
+	return mean, sd, map
+
+def create_dict(map):
+	for row in map:
+		if row[0] in chrom_dict:
+			chrom_dict[row[0]].append(row)
+		else:
+			chrom_dict[row[0]] = [row]
+	return chrom_dict
+#PILFER algorith
+
+def pilfer2(chrom_dict, mean, sd, columnidx):
+	pilfer_result = []
+	for key in chrom_dict:
+		j = 0
+		while j < len(chrom_dict[key]):
+			row = chrom_dict[key][j]
+			if (row[columnidx] - mean)/sd >= sd_factor:
+				start_bp = row[2] - 100000
+				if start_bp < 0:
+					start_bp = 0
+				end_bp = row[2]
+				score = 0
+				new_score = 0
+				start_read_index = -1
+				end_read_index = 0
+				
+				cur_read_index = chrom_dict[key].index(row)
+
+				#Calculate the initial score and start index
+				for read in chrom_dict[key]:
+					if read[1] >= start_bp and read[1] <= start_bp + 100000:
+						score += read[columnidx]
+						if start_read_index == -1:
+							start_read_index = chrom_dict[key].index(read)
+							start_bp = read[1]
+						end_read_index = chrom_dict[key].index(read)
+
+				new_score = score
+				max_start = start_read_index
+				max_end = end_read_index
+				#calculating optimum 100KB window
+				for i in range(start_read_index+1,cur_read_index+1):
+					new_score = score - chrom_dict[key][i-1][columnidx]
+					while  end_read_index+1 < len(chrom_dict[key]) and chrom_dict[key][end_read_index +1][1] <= chrom_dict[key][i][1] + 100000 :
+						new_score += chrom_dict[key][end_read_index+1][columnidx]
+						end_read_index += 1
+					
+					if new_score > score:
+						score = new_score
+						max_start = i
+						max_end = end_read_index
+
+				#print key + "\t" + str(chrom_dict[key][max_start][1]) + "\t" + str(chrom_dict[key][max_end][2]) + "\t" + str(score)
+				#print(key + ":" + str(chrom_dict[key][max_start][1]) + "-" + str(chrom_dict[key][max_end][2]) + "\t" + str(score))
+				pilfer_result.append([key,chrom_dict[key][max_start][1],chrom_dict[key][max_end][2],score])
+				j = max_end
+			j += 1
+	return pilfer_result
+
+
 #Variables
 map = []
 chrom_dict = {}
-columnidx = 6
+columnidx = 4
 
 infile = args.i
 sd_factor = args.f
@@ -30,76 +102,20 @@ else:
 	else:
 		infile_pt = open(infile,"r")
 
-csvin = csv.reader(infile_pt, delimiter = "\t")
 
 #Reading the BED records
-for row in csvin:
-	row[1] = int(row[1])
-	row[2] = int(row[2])
-	row[columnidx] = float(row[columnidx])
-	map.append(row)
-
-#Mean and SD calculations
-mean = np.mean(column(map,columnidx))
-sd = np.std(column(map,columnidx))
+mean, sd, map = read_data_pandas(infile_pt)
 
 #Creating the dictionary
-for row in map:
-	if row[0] in chrom_dict:
-		chrom_dict[row[0]].append(row)
-	else:
-		chrom_dict[row[0]] = [row]
+chrom_dict = create_dict(map)
 
 #Sorting the dictionary
 for key in chrom_dict:
 	chrom_dict[key] = sorted(chrom_dict[key],key=itemgetter(1))
-#del chrom_dict['chrY']
+	
 
-#print "Chromosome\tStart\tEnd\tScore"
-#Cluster calculation
-for key in chrom_dict:
-	j = 0
-	while j < len(chrom_dict[key]):
-		row = chrom_dict[key][j]
-		if (row[columnidx] - mean)/sd >= sd_factor:
-			start_bp = row[2] - 100000
-			if start_bp < 0:
-				start_bp = 0
-			end_bp = row[2]
-			score = 0
-			new_score = 0
-			start_read_index = -1
-			end_read_index = 0
-			
-			cur_read_index = chrom_dict[key].index(row)
+#Rul PILFER2
+pilfer_result = pilfer2(chrom_dict, mean, sd, columnidx)
 
-			#Calculate the initial score and start index
-			for read in chrom_dict[key]:
-				if read[1] >= start_bp and read[1] <= start_bp + 100000:
-					score += read[columnidx]
-					if start_read_index == -1:
-						start_read_index = chrom_dict[key].index(read)
-						start_bp = read[1]
-					end_read_index = chrom_dict[key].index(read)
-
-			new_score = score
-			max_start = start_read_index
-			max_end = end_read_index
-			#calculating optimum 100KB window
-			for i in range(start_read_index+1,cur_read_index+1):
-				new_score = score - chrom_dict[key][i-1][columnidx]
-				while  end_read_index+1 < len(chrom_dict[key]) and chrom_dict[key][end_read_index +1][1] <= chrom_dict[key][i][1] + 100000 :
-					new_score += chrom_dict[key][end_read_index+1][columnidx]
-					end_read_index += 1
-				
-				if new_score > score:
-					score = new_score
-					max_start = i
-					max_end = end_read_index
-
-			#print key + "\t" + str(chrom_dict[key][max_start][1]) + "\t" + str(chrom_dict[key][max_end][2]) + "\t" + str(score)
-			print(key + ":" + str(chrom_dict[key][max_start][1]) + "-" + str(chrom_dict[key][max_end][2]) + "\t" + str(score))
-			j = max_end
-		j += 1
-
-#print index_list
+for row in pilfer_result:
+	print(str(row[0]) + ":" + str(row[1]) + "-" + str(row[2]) + "\t" + str(row[3]))
